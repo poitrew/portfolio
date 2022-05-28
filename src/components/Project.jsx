37ad8978ce00edae22d5493f2ab4db170576fb72
poitrew <dtdat101101@gmail.com>
2022-05-29 03:55:46 +0700

    added public dir for static assets

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ export default function Project({ name, previewImg, link, desc }) {
     return (
         <div className="project">
             <a href={link} target="_blank">
-                <img src={`./src/assets/${previewImg}`} alt="" />
+                <img src={`./${previewImg}`} alt="" />
                 <h1>{name}</h1>
                 <ul>
                     {desc.map((item, index) => <li key={index}>{item}</li>)}
