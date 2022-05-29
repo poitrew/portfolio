7c2b2fdc8609375b805d9ab7d3bac1699dffeb63
poitrew <dtdat101101@gmail.com>
2022-05-29 15:46:12 +0700

    added about section, change the highlight color from yellow to green

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import About from './components/About'
 
 function App() {
 	return (
 		<>
 			<Hero />
+			<About />
 			<Projects />
 			<Contact />
 		</>
